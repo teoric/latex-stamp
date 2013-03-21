5c0f2a23c45e4e499628bbd85a2b18c85328b47f
Bernhard Fisseni <bernhard.fisseni@googlemail.com>
2013-03-21 17:04:37 +0100

    UTF-8 encoding

--- lib/random_memory.rb
+++ lib/random_memory.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class RandomMemory
 
   class RandomMemoryExceeded < StandardError
--- lib/tex_tag.rb
+++ lib/tex_tag.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class TeXTag
     require "babosa"
     @tex_template = <<'EOF'
